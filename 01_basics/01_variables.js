bbbdc76900676101158760cc537ac36a06492e7d
AkashunReaL <142010760+AkashunReaL@users.noreply.github.com>
2024-09-08 07:38:29 +0000

    Studied variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 12234
+let accountEmail = "hcl@gmail.com"
+var accountPassword = "223344"
+
+accountEmail = "lawdakisarkar@gmail.com"
+accountPassword = "9402232966"
+
+// accountId = 13352
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.table([accountId , accountEmail , accountPassword])
+
+
