0f1dad2d5755bb1d334adec26b8bbe1f2154a0b2
AkashunReaL <142010760+AkashunReaL@users.noreply.github.com>
2024-09-11 13:08:05 +0000

    Studied conversion operations

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -8,3 +8,12 @@ let name = "Akash"
 let isLoggedIn = true
 
 console.log(typeof age);
+
+// number => 2 to power 53
+// bigint => for bigger number is rarely used
+// string => akashhhhhhhhhh
+// boolean => true/false
+// NULL => empty
+// undefined => declared but not assigned yet 
+// symbol => unique (will study later)
+// object => big topic comes in later
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score ="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+
+// 1 => true
+// 0 => false
+// "" => false
+// "akash" => true
+
+let someNumber = 33
+
+let StringNumber = String(someNumber)
+console.log(typeof StringNumber);
+console.log(StringNumber)
