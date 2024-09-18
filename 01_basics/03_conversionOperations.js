09ff284fa1f99507aa674d6c9c56ce8e09882fc8
AkashunReaL <142010760+AkashunReaL@users.noreply.github.com>
2024-09-18 08:16:37 +0000

    Studied more conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,11 +1,11 @@
 let score ="33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanisLoggedIn = Boolean(isLoggedIn)
-console.log(booleanisLoggedIn)
+// console.log(booleanisLoggedIn)
 
 // 1 => true
 // 0 => false
@@ -25,5 +25,46 @@ console.log(booleanisLoggedIn)
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log(typeof StringNumber);
-console.log(StringNumber)
+// console.log(typeof StringNumber);
+// console.log(StringNumber)
+
+
+
+// ----------------------OPERATIONS-------------------------
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-1);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+// console.log(2**4);
+
+let str1 = "Hello"
+let str2 = " Akash"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(3 + 4 * 5 % 3);
+
+// console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+/* Go to google and search prefix and postfix js mdn to study more about increment operator */
+// also go to ECMAscript
